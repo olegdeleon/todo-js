ce16c643f8b1335785708a7599cdbcc02b53c2c0
Oleg de Leon <oleg.deleon@gmail.com>
2017-02-17 00:21:44 +0800

    Initial commit

---
+++ assets/js/todo.js
@@ -0,0 +1,26 @@
+// Marks items complete
+$('ul').on('click', 'li', function(){
+  $(this).toggleClass('completed');
+});
+
+// Completely removes items
+$('ul').on('click', 'span', function(event){
+  $(this).parent().fadeOut(300, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+// Add todo
+$('input[type="text"]').on('keypress', function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    $('ul').append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+// Toggle input
+$('.fa-plus').on('click', function(){
+  $('input').fadeToggle();
+});
